225c002f672802e537d86acec368a22f2ec51fa2
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-03-05 00:37:54 -0300

    feat: getStaticProps added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
-import { Button } from '@nextui-org/react';
-import { NextPage } from 'next';
+import { NextPage, GetStaticProps } from 'next';
 import { Layout } from '../components/layouts';
+import { pokeApi } from '../services';
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log(props);
+
   return (
     <Layout title='Listado de Pokemons'>
-      <Button color={'gradient'}>Hola Mundo</Button>
+      <ul>
+        <li>Poke</li>
+        <li>Poke</li>
+        <li>Poke</li>
+        <li>Poke</li>
+        <li>Poke</li>
+        <li>Poke</li>
+      </ul>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get('/pokemon?limit=151');
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
 };
 
 export default HomePage;
