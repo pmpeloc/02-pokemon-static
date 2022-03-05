49cc8631cfca4ee245ee50c27b990be9618db4aa
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-03-05 18:13:37 -0300

    feat: Favorites Page added

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Spacer, Text, useTheme } from '@nextui-org/react';
+import { Spacer, Text, useTheme, Link } from '@nextui-org/react';
 import Image from 'next/image';
+import NextLink from 'next/link';
 
 const Navbar = () => {
   const { theme } = useTheme();
@@ -14,20 +15,32 @@ const Navbar = () => {
         padding: '0px 20px',
         backgroundColor: theme?.colors.gray900.value,
       }}>
-      <Image
-        src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'
-        alt='App icon'
-        width={70}
-        height={70}
-      />
-      <Text color='white' h2>
-        P
-      </Text>
-      <Text color='white' h3>
-        okémon
-      </Text>
+      <NextLink href='/' passHref>
+        <Link>
+          <Image
+            src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'
+            alt='App icon'
+            width={70}
+            height={70}
+          />
+        </Link>
+      </NextLink>
+      <NextLink href='/' passHref>
+        <Link>
+          <Text color='white' h2>
+            P
+          </Text>
+          <Text color='white' h3>
+            okémon
+          </Text>
+        </Link>
+      </NextLink>
       <Spacer css={{ flex: 1 }} />
-      <Text color='white'>Favoritos</Text>
+      <NextLink href='/favorites' passHref>
+        <Link>
+          <Text color='white'>Favoritos</Text>
+        </Link>
+      </NextLink>
     </div>
   );
 };
