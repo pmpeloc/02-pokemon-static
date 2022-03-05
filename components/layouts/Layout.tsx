36e4fe08d3456912613a63455fa7086a265a3d4e
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-03-05 00:21:39 -0300

    feat: Navbar added

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import Head from 'next/head';
 
+import Navbar from '../ui/Navbar';
+
 interface Props {
   title?: string;
 }
@@ -17,7 +19,13 @@ export const Layout: FC<Props> = ({ children, title }) => {
         />
         <meta name='keywords' content={`${title}, pokemon, pokedex`} />
       </Head>
-      <main>{children}</main>
+      <Navbar />
+      <main
+        style={{
+          padding: '0px 20px',
+        }}>
+        {children}
+      </main>
     </>
   );
 };
