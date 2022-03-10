5cf6ff7eeb0c46e606369ef200f56ec467e4ab16
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-03-10 16:32:42 -0300

    feat: Layout favorites

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,11 +1,22 @@
 import { NextPage } from 'next';
+import { Container, Text } from '@nextui-org/react';
 
 import { Layout } from '../../components/layouts';
 
 const FavoritesPage: NextPage = () => {
   return (
     <Layout title='Favoritos'>
-      <h1>Favoritos</h1>
+      <Container
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: 'calc(100vh - 100px',
+          alignItems: 'center',
+          justifyContent: 'center',
+          alignSelf: 'center',
+        }}>
+        <Text h1>No hay favoritos</Text>
+      </Container>
     </Layout>
   );
 };
