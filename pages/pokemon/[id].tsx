c177685d03af50fd95df95d35283aaa6e0eca7f7
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-03-12 17:34:11 -0300

    feat: Canvas Confetti added

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { NextPage, GetStaticProps, GetStaticPaths } from 'next';
 import { Button, Card, Container, Grid, Image, Text } from '@nextui-org/react';
+import confetti from 'canvas-confetti';
 
 import { Layout } from '../../components/layouts';
 import { IPokemon } from '../../interfaces';
@@ -19,6 +20,17 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
   const onToggleFavorite = () => {
     localFavorites.toggleFavorite(pokemon.id);
     setIsInFavorites(!isInFavorites);
+    if (isInFavorites) return;
+    confetti({
+      zIndex: 999,
+      particleCount: 100,
+      spread: 160,
+      angle: -100,
+      origin: {
+        x: 1,
+        y: 0,
+      },
+    });
   };
   return (
     <Layout title={pokemon.name}>
