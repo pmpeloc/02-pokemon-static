60da08fa2ae6d00b64b24fb53f47bbb245d840d3
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-03-12 18:13:49 -0300

    feat: Open Graph Meta Tags added

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -7,6 +7,8 @@ interface Props {
   title?: string;
 }
 
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title }) => {
   return (
     <>
@@ -18,6 +20,15 @@ export const Layout: FC<Props> = ({ children, title }) => {
           content={`Información sobre el Pokemon ${title}`}
         />
         <meta name='keywords' content={`${title}, pokemon, pokedex`} />
+        <meta
+          property='og:title'
+          content={`Información sobre el Pokemon ${title}`}
+        />
+        <meta
+          property='og:description'
+          content={`Esta es la página sobre ${title}`}
+        />
+        <meta property='og:image' content={`${origin}/images/banner.png`} />
       </Head>
       <Navbar />
       <main
