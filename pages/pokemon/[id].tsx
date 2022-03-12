88eb4f92f0202a5c3189dd05e74420140de3ec7a
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-03-12 18:43:03 -0300

    chore: Incremental Static Generation (ISG) added

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -105,13 +105,22 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemons151.map((id) => ({
       params: { id },
     })),
-    fallback: false,
+    // fallback: false,
+    fallback: 'blocking',
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
   const pokemon = await getPokemonInfo(id);
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
   return {
     props: {
       pokemon,
--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,10 +2,14 @@ import { IPokemon } from '../interfaces';
 import { pokeApi } from '../services';
 
 export const getPokemonInfo = async (nameOrId: string) => {
-  const { data } = await pokeApi.get<IPokemon>(`/pokemon/${nameOrId}`);
-  return {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-  };
+  try {
+    const { data } = await pokeApi.get<IPokemon>(`/pokemon/${nameOrId}`);
+    return {
+      id: data.id,
+      name: data.name,
+      sprites: data.sprites,
+    };
+  } catch (error) {
+    return null;
+  }
 };
